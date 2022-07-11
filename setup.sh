d582ad400c1af317c521a764438a8aa24a5ca3f7
Alec Hedlund <alec@parkwestllc.com>
2022-07-10 20:53:38 -0700

    updated setup.sh with theme

--- setup.sh
+++ setup.sh
@@ -1,22 +1,18 @@
 mkdir -p ~/.streamlit/
 
 echo "\
-[server]\n\
-headless = true\n\
-port = $PORT\n\
-enableCORS = false\n\
-\n\
-" > ~/.streamlit/config.toml
-
-echo "\
 [general]\n\
 email = \"your-email@domain.com\"\n\
 " > ~/.streamlit/credentials.toml
 
-echo '[theme]
-base="dark"
-primaryColor="#d33682"
-backgroundColor="#002b36"
-secondaryBackgroundColor="#586e75"
-font="monospace"
-'
+echo "[theme]
+primaryColor=’#d33682’
+backgroundColor=’#002b36’
+secondaryBackgroundColor=’#586e75’
+font = ‘monospace’
+base=‘dark’
+[server]
+headless = true
+port = $PORT
+enableCORS = false
+" > ~/.streamlit/config.toml
