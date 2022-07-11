679da0cc05ed6380e6987f58ace5d751eacac28a
Alec Hedlund <alec@parkwestllc.com>
2022-07-10 20:47:59 -0700

    updated setup.sh with theme

--- setup.sh
+++ setup.sh
@@ -12,3 +12,11 @@ echo "\
 [general]\n\
 email = \"your-email@domain.com\"\n\
 " > ~/.streamlit/credentials.toml
+
+echo '[theme]
+base="dark"
+primaryColor="#d33682"
+backgroundColor="#002b36"
+secondaryBackgroundColor="#586e75"
+font="monospace"
+'
